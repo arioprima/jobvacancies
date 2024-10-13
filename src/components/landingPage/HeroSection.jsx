4be763bae3181df96876e033c219136a2013d773
arioprima <arioprima2@gmail.com>
2024-10-13 21:03:29 +0700

    wip: add add responsive breakpoint for small and medium hero section

--- src/components/landingPage/HeroSection.jsx
+++ src/components/landingPage/HeroSection.jsx
@@ -1,5 +1,6 @@
-import { UilBriefcaseAlt } from '@iconscout/react-unicons'
+import { UilBriefcaseAlt, UilMapMarker } from '@iconscout/react-unicons'
 import Select, { components } from 'react-select';
+import Illustration from '@images/Illustration.svg';
 import PropTypes from 'prop-types';
 
 
@@ -17,7 +18,7 @@ const countryOptions = [
 const Control = (props) => {
     return (
         <components.Control {...props}>
-            <UilBriefcaseAlt color="#82FD6E" className="w-5 h-5" />
+            <UilMapMarker color="#82FD6E" className="w-6 h-6" />
             {props.children}
         </components.Control>
     );
@@ -30,67 +31,78 @@ Control.propTypes = {
 const HeroSection = () => {
     return (
         <div className="flex flex-col pt-16 px-4">
-            <div className="flex flex-col gap-2 text-3xl font-semibold text-slate-700">
-                <p>Find a job that suits</p>
-                <p>your interest & skills.</p>
-            </div>
-            <div className="mt-4 text-lg break-words text-justify text-slate-500">
-                <p>
-                    Find the ideal job for you by exploring the various job openings available
-                    about the company understanding the work culture.
-                </p>
-            </div >
-            <form>
-                <div className="flex flex-col w-full gap-4 mt-10">
-                    <div className="relative">
-                        <UilBriefcaseAlt color="#82FD6E" className="absolute left-4 top-1/2 transform -translate-y-1/2 w-5 h-5" />
-                        <input type="text" placeholder="Job title, Keyword..." className="w-full pl-12 py-3 text-sm bg-[#FFFFFF] focus:outline-none ring-transparent rounded-lg" />
-                    </div>
-                    <div className="relative">
-                        <Select
-                            options={countryOptions}
-                            placeholder="Select your location"
-                            styles={
-                                {
-                                    control: (styles) => ({
-                                        ...styles,
-                                        backgroundColor: 'white',
-                                        border: 'none',
-                                        borderRadius: '0.5rem',
-                                        padding: '0.4rem 1rem',
-                                        fontSize: '0.875rem',
-                                        boxShadow: '0 0 0 1px #D1D5DB',
-                                        '&:hover': {
-                                            borderColor: '#D1D5DB',
-                                        },
-                                    }),
-                                    indicatorSeparator: (styles) => ({
-                                        ...styles,
-                                        display: 'none',
-                                    }),
-                                    dropdownIndicator: (styles) => ({
-                                        ...styles,
-                                        display: 'none',
-                                    }),
-                                    option: (styles, { isFocused }) => ({
-                                        ...styles,
-                                        backgroundColor: isFocused ? '#F3F4F6' : 'white',
-                                        color: '#111827',
-                                        fontSize: '0.875rem',
-                                        padding: '0.75rem',
-                                        '&:hover': {
-                                            backgroundColor: '#F3F4F6',
-                                        },
-                                    }),
-                                }
-                            }
-                            components={{ Control }}
-                            isSearchable={true}
-                            maxMenuHeight={150}
-                        />
-                    </div>
+            <div>
+                <div className="flex flex-col gap-2 text-4xl font-semibold text-slate-700">
+                    <p>Find a job that suits</p>
+                    <p>your interest & skills.</p>
                 </div>
-            </form>
+                <div className="mt-8 text-lg break-words text-justify text-slate-500">
+                    <p>
+                        Find the ideal job for you by exploring the various job openings available
+                        about the company understanding the work culture.
+                    </p>
+                </div >
+                <form>
+                    <div className="flex flex-col w-full gap-4 mt-10">
+                        <div className="relative">
+                            <UilBriefcaseAlt color="#82FD6E" className="absolute left-4 top-1/2 transform -translate-y-1/2 w-5 h-5" />
+                            <input type="text" placeholder="Job title, Keyword..." className="w-full pl-12 py-5 text-sm bg-[#FFFFFF] focus:outline-none ring-transparent rounded-lg" />
+                        </div>
+                        <div className="relative">
+                            <Select
+                                options={countryOptions}
+                                placeholder="Select your location"
+                                styles={
+                                    {
+                                        control: (styles) => ({
+                                            ...styles,
+                                            backgroundColor: 'white',
+                                            border: 'none',
+                                            borderRadius: '0.5rem',
+                                            padding: '0.85rem 1rem',
+                                            fontSize: '0.875rem',
+                                            boxShadow: '0 0 0 1px #D1D5DB',
+                                            '&:hover': {
+                                                borderColor: '#D1D5DB',
+                                            },
+                                        }),
+                                        indicatorSeparator: (styles) => ({
+                                            ...styles,
+                                            display: 'none',
+                                        }),
+                                        dropdownIndicator: (styles) => ({
+                                            ...styles,
+                                            display: 'none',
+                                        }),
+                                        option: (styles, { isFocused }) => ({
+                                            ...styles,
+                                            backgroundColor: isFocused ? '#F3F4F6' : 'white',
+                                            color: '#111827',
+                                            fontSize: '0.875rem',
+                                            padding: '0.75rem',
+                                            '&:hover': {
+                                                backgroundColor: '#F3F4F6',
+                                            },
+                                        }),
+                                    }
+                                }
+                                components={{ Control }}
+                                isSearchable={true}
+                                maxMenuHeight={150}
+                            />
+                        </div>
+                        <button className="btn btn-success w-full py-4 text-sm hover:bg-green-400">Search Jobs</button>
+                    </div>
+                </form>
+                <div className="mt-4 text-sm text-slate-500">
+                    <p>
+                        <strong>Suggestion:</strong> Designer, Programming, Digital Marketing, Video, Amimation, etc.
+                    </p>
+                </div >
+            </div>
+            <div >
+                <img src={Illustration} alt="hero" className="w-full  my-10" />
+            </div>
         </div >
     );
 };
