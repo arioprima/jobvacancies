7613d741bc40bc6fd26f5f21b47bc01ec489fcef
Ario Prima <arioprima2@gmail.com>
2025-02-11 23:36:26 +0700

    wip: add find job page

---
+++ src/pages/Jobs/Jobs.jsx
@@ -0,0 +1,12 @@
+const Jobs = () => {
+    return (
+        <div className="flex flex-col pt-16 px-4 lg:flex-row lg:pt-8 lg:px-16">
+            <div className="lg:w-[25%]  bg-red-500">sdsd</div>
+            <div className="lg:w-[75%]  bg-blue-500">
+
+            </div>
+        </div>
+    )
+}
+
+export default Jobs
