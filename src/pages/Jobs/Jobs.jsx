2b4bf9739a186562573f59238412fb32e9c4367a
Ario Prima <arioprima2@gmail.com>
2025-02-23 23:19:29 +0700

    wip: add accordion for filter

--- src/pages/Jobs/Jobs.jsx
+++ src/pages/Jobs/Jobs.jsx
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import Select, { components } from 'react-select';
 import { UilBriefcaseAlt, UilMapMarker } from '@iconscout/react-unicons';
+import AccordionFilter from './components/AccordionFilter';
 
 const countryOptions = [
     { value: 'us', label: 'United States' },
@@ -13,6 +14,40 @@ const countryOptions = [
     { value: 'jp', label: 'Japan' },
 ];
 
+const filters = [
+    {
+        id: 0,
+        title: "Prioritise By",
+        options: ["Most Recent", "Most Relevant", "Highest Salary", "Most Popular"]
+    },
+    {
+        id: 1,
+        title: "Job Type",
+        options: ["Full-time", "Contract", "Internship", "Freelance", "Part-time", "Daily"]
+    },
+    {
+        id: 2,
+        title: "Work Arrangement Options",
+        options: ["Onsite", "Hybrid", "Remote/WFH"]
+    },
+    {
+        id: 3,
+        title: "Experience",
+        options: ["No Cxperience", "Fresh Graduate", "< 1 Year", "1-2 Years", "2-3 Years", "3-5 Years", "5-10 Years", "> 10 Years"]
+    },
+    {
+        id: 4,
+        title: "Education Level",
+        options: ["PhD", "Master's Degree", "Bachelor's Degree", "Diploma", "Senior/Vocational High School", "Secondary School", "Elementary School"]
+    },
+    {
+        id: 5,
+        title: "Last Updated",
+        options: ["Past Week", "Past 24 Hours", "Any Time", "Past Month"]
+    }
+];
+
+
 const Control = (props) => {
     return (
         <components.Control {...props}>
@@ -22,9 +57,11 @@ const Control = (props) => {
     );
 };
 
+
+
 const Jobs = () => {
     return (
-        <div className="flex flex-col pt-16 px-4 lg:pt-8 lg:px-16">
+        <div className="flex flex-col pt-16 px-4 lg:pt-8 lg:px-16 gap-10 pb-16">
             <form>
                 <div className="flex gap-5 ">
                     <div className="relative w-full">
@@ -89,7 +126,21 @@ const Jobs = () => {
                 </div>
             </form>
 
-            <div>tesging2</div>
+            <div className="grid grid-cols-1 md:grid-cols-12 gap-10">
+                <div className="border-2 rounded-lg md:col-span-4">
+                    <div className=" md:col-span-3 overflow-y-scroll max-h-[80vh]">
+                        {filters.map((filter) => (
+                            <AccordionFilter key={filter.id} title={filter.title} options={filter.options} />
+                        ))}
+                    </div>
+
+                </div>
+                <div className="bg-yellow-300 md:col-span-8">
+                    Card Jobs
+                </div>
+            </div>
+
+
         </div>
     );
 };
